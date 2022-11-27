bc1f76ff517ce71289f7a4a09e6b00ebe5c14af0
Guilherme Vicente <gui.vcnt18@gmail.com>
2022-11-27 20:31:18 -0300

    Finalização do trabalho de algoritmos

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_PALAVRA_SIZE 20
+#define TAMANHO_VETOR 20
+
 
 typedef struct FrequenciaPalavra {
     char palavra[MAX_PALAVRA_SIZE];
@@ -37,6 +40,8 @@ int main() {
     struct FrequenciaPalavraLookup palavraLookup[20] = {NULL, 0};
     struct FrequenciaPalavra frequenciaPalavra[200] = {NULL, 0};
 
+
+    int sizearray;
     int i = 0;
     char palavra[MAX_PALAVRA_SIZE] = {0};
     while (fscanf(arquivoPtr, "%s", palavra) == 1) {
@@ -47,92 +52,117 @@ int main() {
             strcpy( palavraLookup[i].palavra,palavra);
             palavraLookup[i].index = i;
             i++;
+            sizearray++;
             continue;
         }
         frequenciaPalavra[frequenciaPalavraIndex].frequencia += 1;
     }
 
-    int opcaoMenu;
-    printf("Selecione a opcao desejada\n");
-    printf("1 - Consultar a quantidade de ocorrencias de uma determinada palavra no arquivo\n");
-    printf("2 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem crescente de ocorrencia\n");
-    printf("3 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem decrescente de ocorrencia\n");
-    printf("4 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem alfabetica\n");
-    printf("5 - Listar todas as palavras que constam no arquivo, e que iniciam por um caractere informado pelo usuario, bem como a quantidade de vezes que essas palavras ocorrem, em ordem crescente de ocorrencia\n");
-    printf("6 - Encerrar o programa\n");
-    scanf("%d", &opcaoMenu);
+
 
     char palavraencontrada[10];
     char caracterBusca;
-
-
     int g;
     int v;
     struct FrequenciaPalavra temp;
-    switch (opcaoMenu) {
-        case 1:
-            printf("Digite a palavra a ser buscada:");
-            scanf("%s", palavraencontrada);
-            for(int j=0;j<11;j++){
 
-                if(!strcmp(frequenciaPalavra[j].palavra, palavraencontrada)) {
-                    printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
-                    continue;
-                }
-        }
-            break;
-        case 2:
-            for(g=0;g<11-1;g++){
-                for(v=0;v<(11-1-g);v++){
-                    if(frequenciaPalavra[v].frequencia > frequenciaPalavra[v+1].frequencia){
-                        temp = frequenciaPalavra[v];
-                        frequenciaPalavra[v] = frequenciaPalavra[v+1];
-                        frequenciaPalavra[v+1] = temp;
+        int opcaoMenu;
+        printf("1 - Consultar a quantidade de ocorrencias de uma determinada palavra no arquivo\n");
+        printf("2 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem crescente de ocorrencia\n");
+        printf("3 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem decrescente de ocorrencia\n");
+        printf("4 - Listar todas as palavras que constam no arquivo, bem como a quantidade de vezes que elas ocorrem, em ordem alfabetica\n");
+        printf("5 - Listar todas as palavras que constam no arquivo, e que iniciam por um caractere informado pelo usuario, bem como a quantidade de vezes que essas palavras ocorrem, em ordem crescente de ocorrencia\n");
+        printf("6 - Encerrar o programa\n");
+
+    while(true) {
+        printf("\nSelecione a opcao desejada\n");
+        scanf("%d", &opcaoMenu);
+
+        switch (opcaoMenu) {
+            case 1:
+                printf("Digite a palavra a ser buscada \n");
+                scanf("%s", palavraencontrada);
+                for (int j = 0; j < sizearray; j++) {
+                    if (!strcmp(frequenciaPalavra[j].palavra, palavraencontrada)) {
+                        printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
+                        continue;
                     }
                 }
-            }
-            for(int j=0;j<11;j++){
-                printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
-            }
 
-            break;
-        case 3:
-            for(g=0;g<11-1;g++){
-                for(v=0;v<(11-1-g);v++){
-                    if(frequenciaPalavra[v].frequencia < frequenciaPalavra[v+1].frequencia){
-                        temp = frequenciaPalavra[v];
-                        frequenciaPalavra[v] = frequenciaPalavra[v+1];
-                        frequenciaPalavra[v+1] = temp;
-                    }
-                }
-            }
-            for(int j=0;j<11;j++){
-                printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
-            }
-            break;
-        case 4:
-            for(int w=0; w<13;w++) {
-                for (int i = 0; i <= 9; ++i) {
-                    for (int k = 0; frequenciaPalavra[i].palavra[k] != '\0'; ++k) {
-                        if ((frequenciaPalavra[i].palavra[k] > frequenciaPalavra[i + 1].palavra[k])) {
-                            temp = frequenciaPalavra[i];
-                            frequenciaPalavra[i] = frequenciaPalavra[i + 1];
-                            frequenciaPalavra[i + 1] = temp;
-                            break;
-                        } else if ((frequenciaPalavra[i].palavra[k] < frequenciaPalavra[i + 1].palavra[k])) {
-                            break;
+                break;
+            case 2:
+                for (g = 0; g < sizearray - 1; g++) {
+                    for (v = 0; v < (sizearray - 1 - g); v++) {
+                        if (frequenciaPalavra[v].frequencia > frequenciaPalavra[v + 1].frequencia) {
+                            temp = frequenciaPalavra[v];
+                            frequenciaPalavra[v] = frequenciaPalavra[v + 1];
+                            frequenciaPalavra[v + 1] = temp;
                         }
                     }
                 }
-            }
-            for(int j=0;j<11;j++){
-                printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
-            }
-            break;
-        case 5:
-            printf("Digite o caracter a ser procurado \n");
-            scanf("%c", caracterBusca);
-            break;
-        case 6: return 1;
+                for (int j = 0; j < sizearray; j++) {
+                    printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
+                }
+
+                break;
+            case 3:
+                for (g = 0; g < sizearray - 1; g++) {
+                    for (v = 0; v < (sizearray - 1 - g); v++) {
+                        if (frequenciaPalavra[v].frequencia < frequenciaPalavra[v + 1].frequencia) {
+                            temp = frequenciaPalavra[v];
+                            frequenciaPalavra[v] = frequenciaPalavra[v + 1];
+                            frequenciaPalavra[v + 1] = temp;
+                        }
+                    }
+                }
+                for (int j = 0; j < sizearray; j++) {
+                    printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
+                }
+                break;
+            case 4:
+                for (int w = 0; w < 13; w++) {
+                    for (int i = 0; i <= 9; ++i) {
+                        for (int k = 0; frequenciaPalavra[i].palavra[k] != '\0'; ++k) {
+                            if ((frequenciaPalavra[i].palavra[k] > frequenciaPalavra[i + 1].palavra[k])) {
+                                temp = frequenciaPalavra[i];
+                                frequenciaPalavra[i] = frequenciaPalavra[i + 1];
+                                frequenciaPalavra[i + 1] = temp;
+                                break;
+                            } else if ((frequenciaPalavra[i].palavra[k] < frequenciaPalavra[i + 1].palavra[k])) {
+                                break;
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < sizearray; j++) {
+                    printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j].palavra);
+                }
+                break;
+            case 5:
+                printf("Digite o caracter a ser procurado \n");
+                scanf(" %c", &caracterBusca);
+
+                for (g = 0; g < sizearray - 1; g++) {
+                    for (v = 0; v < (sizearray - 1 - g); v++) {
+                        if (frequenciaPalavra[v].frequencia > frequenciaPalavra[v + 1].frequencia) {
+                            temp = frequenciaPalavra[v];
+                            frequenciaPalavra[v] = frequenciaPalavra[v + 1];
+                            frequenciaPalavra[v + 1] = temp;
+                        }
+                    }
+                }
+
+                for (int j = 0; j < sizearray; ++j) {
+                    if (caracterBusca == frequenciaPalavra[j].palavra[0]) {
+                        printf("%d:%s\n", frequenciaPalavra[j].frequencia, frequenciaPalavra[j]);
+
+                    }
+                }
+                break;
+            case 6:
+                printf("Finalizando o programa.");
+                return 1;
+        }
     }
-}
+    }
+
